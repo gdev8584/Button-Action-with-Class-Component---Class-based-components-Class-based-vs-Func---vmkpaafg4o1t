8746bcc278dd10963953ce0b8ded8cadce1b2b6f
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-09-09 20:03:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     constructor(props) {
 		super(props);
 		this.state={
+      			id:"",
 			text:""
 		}
 	};
@@ -12,12 +13,13 @@ class App extends Component {
     render() {
 		const handleClick=()=>{
 			this.setState({
+        			id:'para',
 				text:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
 			})
 		}
     	return(
     		<div id="main">
-				<p id='para'>{this.state.text}</p>
+				<p id={this.state.id}>{this.state.text}</p>
 				<button id='click' onClick={handleClick}>Click</button>
     		</div>
     	);
